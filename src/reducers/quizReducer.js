764792ba27fb8d63eac1c4bba86849111067e48b
kenhughlee <kenhughlee@yahoo.com>
2018-01-10 13:11:48 -0800

    added reducer for the quiz

---
+++ src/reducers/quizReducer.js
@@ -0,0 +1,37 @@
+initialState = {
+  quiz: [
+        {
+          question: "5 * 4 =",
+          answers: [
+          "sadsfafd","asdfasdf"
+          ]
+      },
+      {
+        question: "5 + 5 =",
+        answers: [
+          "asdfasdf","asdsfasdfsd"
+        ]
+      }
+  ],
+  index: 0,
+  numberOfQuestions: 7,
+  score: 0,
+  solution:0,
+  response:0,
+  completed: false,
+  correct:-1
+}
+
+const quizReducer = (state=initialState,action) => {
+  switch (action.type) {
+  case 'HANDLE_SUBMIT':
+    return action.payload;
+
+  case 'HANDLE_ANSWER':
+    return {...state,response:action.payload};
+
+  default: return state;
+  }
+}
+
+export default quizReducer;
