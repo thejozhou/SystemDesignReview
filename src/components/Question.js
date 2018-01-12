97469d4472ccac78e6c6429dbab867b8a56300c3
kenhughlee <kenhughlee@yahoo.com>
2018-01-12 14:03:21 -0800

    changed to redux style props

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,13 +5,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
-class Question extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { correct, question, index, onAnswerSelected, onSubmit } = this.props;
+const Question = props => {
     const styles = {
       block: {
         maxWidth: 250,
@@ -30,51 +24,51 @@ class Question extends Component {
       }
     };
 
-    function rightOrWrong() {
+    function rightOrWrong(correct) {
         if (correct===1)
-        return <div>Correct Answer!</div>
+        return (<div>Correct Answer!</div>)
         else if (correct===2)
-        return <div>Incorrect Answer!</div>
+        return (<div>Incorrect Answer!</div>)
     }
 
     return (
       <MuiThemeProvider>
         <div>
           <span className='question'>
-            <h1 className="questionText">{question.question}</h1>
+            <h1 className="questionText">{props.question.question}</h1>
           </span>
-          <RadioButtonGroup name="shipSpeed" defaultSelected="not_light" onChange={onAnswerSelected}>
+          <RadioButtonGroup name="shipSpeed" defaultSelected="not_light" onChange={props.onAnswerSelected}>
             <RadioButton
               value={0}
-              label={question.answers[0]}
+              label={props.question.answers[0]}
               style={styles.radioButton}
               labelStyle={styles.label}
             />
             <RadioButton
               value={1}
-              label={question.answers[1]}
+              label={props.question.answers[1]}
               style={styles.radioButton}
               labelStyle={styles.label}
             />
             <RadioButton
               value={2}
-              label={question.answers[2]}
+              label={props.question.answers[2]}
               style={styles.radioButton}
               labelStyle={styles.label}
             />
             <RadioButton
               value={3}
-              label={question.answers[3]}
+              label={props.question.answers[3]}
               style={styles.radioButton}
               labelStyle={styles.label}
             />
           </RadioButtonGroup>
-          {rightOrWrong}
-          <RaisedButton label="Submit" primary={true} style={styles.submit} onClick={onSubmit} />
+          {rightOrWrong(props.correct)}
+          <RaisedButton label="Submit" primary={true} style={styles.submit} onClick={props.onSubmit} />
         </div>
       </MuiThemeProvider>
     )
   }
-}
+
 
 export default Question
