0a6898962e00adc00ffb5dd879ef9ffef59ac032
scottrosen14 <scott.rosen14@gmail.com>
2018-01-29 19:48:29 -0800

    removed composeWithDevTools from index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension'
 import {createStore, applyMiddleware} from 'redux';
 
 import allReducers from './reducers/index';
 import App from './containers/App.js';
 
 const store = createStore(
-    allReducers,
-    composeWithDevTools()
+    allReducers
+    // composeWithDevTools()
 );
 
 ReactDom.render(
